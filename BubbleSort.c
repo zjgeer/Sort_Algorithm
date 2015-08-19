38eabe5495637375cd83047e776e0113e49e68f4
zjgeer <904031659@qq.com>
2015-08-19 21:52:59 +0800

    Add two c file BubbleSort.c and HeapSort.c

---
+++ BubbleSort.c
@@ -0,0 +1,151 @@
+// suanfa1.cpp : Defines the entry point for the console application.
+//
+#include "stdafx.h"
+
+#include "stdlib.h"
+#include "stdio.h"
+#include "time.h"
+
+
+/* 用于排序数组个数最大值，可以根据需要修改 */
+#define MAXSIZE 1000
+/* 排序数据的结构体 */
+typedef struct
+{
+	int r[MAXSIZE + 1];	/* 存储排序数组 */
+	int length;			/* 用于记录顺序表的长度 */
+}sqList;
+
+void swap(sqList *L,int i,int j)
+{
+	int temp = L->r[i];
+
+	L->r[i] = L->r[j];
+	L->r[j] = temp;
+}
+
+int myrand()
+{
+	srand( (unsigned)time( NULL ) ); /*将当前时间设置成随机函数的种子，所以每次产生的数都不一样*/
+	return rand();
+}
+
+
+
+void BubbleSort0(sqList *L)
+{
+	int i,j;
+	float swCounter = 0;
+
+	for(i = 0;i < L->length - 1;i++)
+	{
+		for(j = i + 1;j < L->length;j++)
+		{
+			if(L->r[i] > L->r[j])
+			{
+				swap(L,i,j);
+				swCounter ++;
+			}
+		}
+	}
+	printf("\nThe bubbleSort0 swap counter is %f.\n",swCounter);
+}
+
+void BubbleSort1(sqList *L)
+{
+	int i,j;
+	float swCounter1 = 0;
+	char flag = 1;
+
+	for(i = 0; i < L->length && flag;i++)
+	{
+		flag = 0;
+		for(j = L->length-2;j >= i;j--)
+		{
+			if(L->r[j] > L->r[j+1])
+			{
+				swap(L,j,j+1);
+				swCounter1 ++;
+				flag = 1;
+			}
+		}
+	}
+
+	printf("\nThe bubbleSort1 swap counter is %f.\n",swCounter1);
+	
+}
+
+
+void SelectSort(sqList *L)
+{
+	int i,j,min;
+	float swCounter = 0;
+	for(i = 0;i < L->length - 1;i++)
+	{
+		min = i;
+
+		for(j = i+1;j < L->length;j++)
+		{
+			if(L->r[min] > L->r[j])
+			{
+				min = j;
+			}
+		}
+		if(i != min)
+		{
+			swap(L,i,min);
+			swCounter++;
+
+		}
+	}
+	printf("\nThe SelectSort swap counter is %f.\n",swCounter);
+}
+
+//冒泡排序的两个方法
+
+int main(int argc, char* argv[])
+{
+	int counter;
+	srand( (unsigned)time( NULL ) ); /*将当前时间设置成随机函数的种子，所以每次产生的数都不一样*/
+	sqList mysqList,mysqList1;
+	
+	mysqList.length = MAXSIZE;
+	mysqList1.length = MAXSIZE;
+
+
+	for(counter = 0;counter<mysqList.length;counter++)
+	{
+		mysqList.r[counter] = rand();
+		mysqList1.r[counter] = mysqList.r[counter];
+	}
+
+
+
+	printf("\nThe original data is :\n");	
+	for(counter = 0;counter < mysqList.length;counter++)
+	{
+
+		printf("%d: %d \n",counter,mysqList.r[counter]);
+	}
+
+	BubbleSort0(&mysqList);
+//	BubbleSort1(&mysqList1);
+	SelectSort(&mysqList1);
+
+/*	printf("\nThe bubble sort result is :\n");	
+	for(counter = 0;counter < mysqList.length;counter++)
+	{
+
+		printf("%d: %d \n",counter,mysqList.r[counter]);
+	}
+
+	printf("\nThe Simple sort result is :\n");	
+	for(counter = 0;counter < mysqList1.length;counter++)
+	{
+
+		printf("%d: %d \n",counter,mysqList1.r[counter]);
+	}
+*/
+	return 0;
+}
+
---
+++ HeapSort.c
@@ -0,0 +1,90 @@
+// HeapSort_1.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include "stdio.h"
+#include "stdlib.h"
+#include "time.h"
+
+#define MAXSIZE 10
+
+typedef struct 
+{
+	int r[MAXSIZE];
+	int length;
+}sqList;
+
+void Swap(sqList *L,int i,int j)
+{
+	int temp = L->r[i];
+	L->r[i] = L->r[j];
+	L->r[j] = temp;
+}
+
+void HeapAdjust(sqList *L,int s,int m)
+{
+	int j,temp;
+
+	temp = L->r[s];
+
+	for(j = 2 * s;j <= m;j++)
+	{
+		if(j < m && L->r[j] < L->r[j + 1])	/* 如果左右子树都存在判断最大是哪一个 */
+		{
+			++j;
+		}
+		
+		if(temp >= L->r[j])					/* 判断父节点与两个子树大小 */
+		{
+			break;							/* 已满足堆的定义，可以退出调整 */
+		}
+		L->r[s] = L->r[j];					/* 将最大值赋值给堆顶 */
+		s = j;
+	}
+	L->r[s] = temp;							/* 将最小值赋值给子树 */
+}
+
+void HeapSort(sqList *L)
+{
+	int i;
+
+	for(i = L->length/2;i > 0;i--)			/* 将数组元素调整为大顶堆 */
+	{
+		HeapAdjust(L,i,L->length);
+	}
+
+	for(i = L->length;i > 1;i--)			/* 取出大顶堆的堆顶，放在碓尾，重新调整剩余的为大顶堆 */
+	{
+		Swap(L,1,i);
+		HeapAdjust(L,1,i-1);
+	}
+}
+
+
+int main(int argc, char* argv[])
+{
+	int i;
+	sqList myList;
+
+	srand((unsigned)time(NULL));
+	
+	myList.length = MAXSIZE - 1;
+	myList.r[0] = 0;
+	
+	for( i = 1;i <MAXSIZE;i++)
+	{
+		myList.r[i] = rand()%100;
+	}
+
+	HeapSort(&myList);
+
+	printf("The heap sort result is :\n");
+
+	for(i = 1;i < MAXSIZE;i++)
+	{
+		printf("%d=%d  ",i,myList.r[i]);
+	}
+
+	return 0;
+}
+
